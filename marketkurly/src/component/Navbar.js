0ace2befd824f70146fbb36a042f460fc4f907e8
heosooyeon <heosoo0806@gmail.com>
2022-06-16 14:46:37 +0900

    메뉴바 작성

--- marketkurly/src/component/Navbar.js
+++ marketkurly/src/component/Navbar.js
@@ -5,14 +5,14 @@ export default function NavBar() {
         <div className="header">
             <div id="userMenu">
                 <ul class="list_menu">
-                    <li className="">
-                        <a className="link_menu">회원가입</a>
+                    <li class ="menu none_sub menu_join">
+                    <a className="link_menu" href="/shop/memder/join.php" class="link_menu">회원가입</a></li>
+                    <li class="menu none_sub menu_login">
+                        <a className="link_menu" href="/shop/memder/login.php" class="link_menu">로그인</a>
                     </li>
-                    <li>
-                        <a className="link_menu">로그인</a>
-                    </li>
-                    <li>
-                        <a className="link_menu">고객센터</a>
+                    <li class="menu lst">
+
+                        <a className="link_menu" href="shop/board/list.php?id=notice" class="link_menu">고객센터</a>
                     </li>
                 </ul>
             </div>
