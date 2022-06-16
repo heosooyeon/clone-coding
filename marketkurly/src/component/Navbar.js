41796bd6e700667fd6b6f51b6960e64c1060b8d8
heosooyeon <heosoo0806@gmail.com>
2022-06-16 15:52:40 +0900

    아이콘 사진 삽입

--- marketkurly/src/component/Navbar.js
+++ marketkurly/src/component/Navbar.js
@@ -19,6 +19,19 @@ export default function NavBar() {
             <br></br>
             <span id="gnbLogoContainer"></span>
             <h1 className="logo"><img className="NavBarImg" src="./img/로고-아이콘.jpg"></img></h1>
+            <div>
+                <ul>
+                    <li><img src='./img/메뉴.png'></img><a>전체 카테고리</a></li>
+                    <li><a>신상품</a></li>
+                    <li><a>베스트</a></li>
+                    <li><a>알뜰쇼핑</a></li>
+                    <li><a>특가/혜택</a></li>
+                    <li><div><input placeholder="검색어를 입력해주세요."></input><img src='./img/검색-아이콘.png'></img></div></li>
+                    <li><button><img src='./img/지도-아이콘.png'></img></button></li>
+                    <li><button><img src='./img/하트-아이콘.png'></img></button></li>
+                    <li><button><img src='./img/장바구니-아이콘.png'></img></button></li>
+                </ul>
+            </div>
         </div>
     )
 }
