60a4a343b37c135d1d38c7df42851f37c0662ab8
heosooyeon <heosoo0806@gmail.com>
2022-06-16 15:29:24 +0900

    colorjoin

--- marketkurly/src/component/Navbar.js
+++ marketkurly/src/component/Navbar.js
@@ -6,16 +6,17 @@ export default function NavBar() {
             <div id="userMenu">
                 <ul class="list_menu">
                     <li class ="menu none_sub menu_join">
-                    <a className="link_menu" href="/shop/memder/join.php" class="link_menu">회원가입</a></li>
+                    <a className="link_menu_join" >회원가입</a>
+                    </li>
                     <li class="menu none_sub menu_login">
-                        <a className="link_menu" href="/shop/memder/login.php" class="link_menu">로그인</a>
+                        <a className="link_menu_login"  >로그인</a>
                     </li>
                     <li class="menu lst">
-
-                        <a className="link_menu" href="shop/board/list.php?id=notice" class="link_menu">고객센터</a>
+                        <a className="link_menu_lst"  >고객센터</a>
                     </li>
                 </ul>
             </div>
+            <br></br>
             <span id="gnbLogoContainer"></span>
             <h1 className="logo"><img className="NavBarImg" src="./img/로고-아이콘.jpg"></img></h1>
         </div>
