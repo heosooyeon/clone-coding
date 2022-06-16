a630d80242672134da90db012c7c0c3c1c6c8932
DONGHAEY <DONGHEAEY@gmail.com>
2022-06-16 14:08:48 +0900

    css edit

--- marketkurly/src/component/Navbar.js
+++ marketkurly/src/component/Navbar.js
@@ -2,8 +2,9 @@
 
 export default function NavBar() {
     return (
-        <div className="NavBar">
-            <img className="NavBarImg" src="./img/로고-아이콘.jpg"></img>
+        <div className="header">
+            <span id="gnbLogoContainer"></span>
+            <h1 className="logo"><img className="NavBarImg" src="./img/로고-아이콘.jpg"></img></h1>
         </div>
     )
 }
